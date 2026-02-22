dd1c6ba0c41af3757abf50e9fac77d640ec94248
carlii28 <carlanaomi.lazondiaz@usp.ceu.es>
2026-02-22 23:45:28 +0100

    Update LecturaSensor.java

--- src/LecturaSensor.java
+++ src/LecturaSensor.java
@@ -1,5 +1,38 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import java.time.LocalDateTime;
+
+public class LecturaSensor {
+
+    private int id;
+    private double valor;
+    private String tipo;
+    private LocalDateTime timestamp;
+
+    public LecturaSensor(int id, double valor, String tipo, LocalDateTime timestamp) {
+        this.id = id;
+        this.valor = valor;
+        this.tipo = tipo;
+        this.timestamp = timestamp;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    @Override
+    public String toString() {
+        return  "id =" + id + ", valor =" + valor + ", tipo ='" + tipo + ", timestamp =" + timestamp;
     }
 }
+
